eae16507ffda6ae9d3827aae314fd9261b65bbe5
JanineDG <46197146+JanineAira@users.noreply.github.com>
2022-09-19 21:35:59 +0800

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,12 +1,13 @@
 class BudgetList():
     def __init__(self, budget):
         self.budget = budget
-        self.sum_expense = 0
+        self.sum_expenses = 0
         self.expenses = []
         self.sum_overages = 0
         self.overages = []
 
         def append(self, item):
-            if (self.sum_expenses + item) < self.budget:
-                self.expenses.append(item)
-                self.sum_expenses += item
+            pass
+            # if (self.sum_expenses + item) < self.budget:
+            #     self.expenses.append(item)
+            #     self.sum_expenses += item
