1660a92c6be1bb90378c4e03824831b79dd4c0db
JanineDG <46197146+JanineAira@users.noreply.github.com>
2022-09-20 20:18:09 +0800

    Completed module

--- budget/Expense.py
+++ budget/Expense.py
@@ -43,9 +43,9 @@ class Expenses():
 
     def categorize_set_comprehension(self):
         necessary_expenses = {x for x in self.list
-            if x.category == 'Phone' or x.category == 'Auto and Gas' 
-            or x.category == 'Classes' or x.category == 'Utilise' 
-            or x.category == 'Mortgage'}
+            if x.category == 'Phone' or x.category == 'Auto and Gas' or 
+            x.category == 'Classes' or x.category == 'Utilities' or 
+            x.category == 'Mortgage'}
 
         food_expenses = {x for x in self.list
             if x.category == 'Groceries' or x.category == 'Eating Out'}
